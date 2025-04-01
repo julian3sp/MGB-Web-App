e215f4ac44f322c8dd7c284a0e67a91d478b3632
julian3sp <julianrespinal24@gmail.com>
2025-03-31 21:10:47 -0400

    figuring out how to hardcode postgres inserts

--- apps/backend/src/bin/prisma-client.ts
+++ apps/backend/src/bin/prisma-client.ts
@@ -3,5 +3,19 @@ import { PrismaClient } from 'database';
 // Create the prisma client, this automatically connects to the database
 const client = new PrismaClient();
 
+async function insertEmployee(name: string){
+    try{
+        const newEmployee = await client.employee.create({
+            data: {
+                employee_name: name, // Ensure `employee_name` exists in your Prisma model
+            },
+        });
+
+        }
+    catch{
+
+    }
+}
+
 // Export the client
 export default client;

1af4e64a968bde147324bcfa328e0347cdbf5bbf
wheeler-bryan <bswheeler@wpi.edu>
2025-04-02 13:10:09 -0400

    changed to tailwind, formatted submit button

--- apps/frontend/src/routes/SignInPage.tsx
+++ apps/frontend/src/routes/SignInPage.tsx
@@ -4,11 +4,9 @@ import {SignInForm} from "../components/signIn/SignInForm.tsx";
 
 export default function SignInPage() {
     return (
-        <div id="signInBackground">
-            <div className="m-auto p-[42px_72px] bg-white rounded-[30px] w-[540px] h-[496px]">
-                <div className={"SignInForm"}>
-                    <SignInForm/>
-                </div>
+        <div className="flex justify-center items-center w-screen h-screen bg-[radial-gradient(circle_at_top_left,white,#003a96)]">
+            <div className="m-auto py-[42px] px-[72px] bg-[white] rounded-[30px] w-[540px] h-[516px]">
+                <SignInForm/>
             </div>
         </div>
     )

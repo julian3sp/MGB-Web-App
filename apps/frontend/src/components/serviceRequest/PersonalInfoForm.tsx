246b50a9b99c7f9c1a9ae726587e64f7c72bb8d9
julian3sp <julianrespinal24@gmail.com>
2025-04-02 00:10:22 -0400

    rounded corners fix

--- apps/frontend/src/components/serviceRequest/PersonalInfoForm.tsx
+++ apps/frontend/src/components/serviceRequest/PersonalInfoForm.tsx
@@ -5,42 +5,48 @@ function PersonalInformationForm() {
         <>
             <div >
 
-                <form className="flex justify-center gap-2 mb-3 ">
-                    <table className="w-[800px] mx-auto text-center border-1 border-black rounded-md  border-collapse">
+                <form className="flex justify-center  gap-2 mb-3 ">
+                    <div className={"border-2 rounded-lg shadow-lg overflow-hidden w-[800px]"}>
+                    <table className="w-[801px] mx-auto text-center  border-black text-sm rounded-lg ">
                         <tr>
-                            <th colSpan={2} className="text-center py-4 text-xl  font-semibold bg-[#003a96] text-white ">
+                            <th colSpan={2} className="text-center py-4 text-xl  font-semibold bg-[#003a96] text-white rounded-tr-sm rounded-tl-sm ">
                                 Section 1: Employee Information
                             </th>
                         </tr>
                         <tr>
-                            <td className="p-6 bg-white">
+                            <td className="p-6 bg-white text-left">
+                                <label htmlFor={"email"} >Email:</label>
                                 <input
-                                    className="bg-white border border-gray-300 p-5 h-10 w-full rounded-md hover:border-[#a2caff] focus:outline-none"
-                                    placeholder="Insert email..."
+                                    className="bg-white  border border-gray-300 p-5 h-10 w-full rounded-md hover:border-[#a2caff] focus:outline-none"
+                                    placeholder="Insert email..." id={"email"}
                                 />
                             </td>
-                            <td className="p-6 bg-white">
+                            <td className="p-6 bg-white text-left">
+                                <label htmlFor={"name"} >Enter Name:</label>
                                 <input
                                     className="bg-white border border-gray-300 p-5 h-10 w-full rounded-md hover:border-[#a2caff] focus:outline-none"
-                                    placeholder="Insert full name..."
+                                    placeholder="Insert full name..." id={"name"}
                                 />
                             </td>
                         </tr>
                         <tr>
-                            <td className="p-6 bg-white">
+                            <td className="p-6 bg-white text-left">
+                                <label htmlFor={"phone"} >Phone:</label>
                                 <input
                                     className="bg-white border border-gray-300 p-5 h-10 w-full rounded-md hover:border-[#a2caff] focus:outline-none"
-                                    placeholder="Insert phone number..."
+                                    placeholder="Insert phone number..." id={"phone"}
                                 />
                             </td>
-                            <td className="p-6 bg-white">
+                            <td className="p-6 bg-white text-left">
+                                <label className={"p-1"} htmlFor={"empID"} >Employee ID:</label>
                                 <input
                                     className="bg-white border border-gray-300 p-5 h-10 w-full rounded-md hover:border-[#a2caff] focus:outline-none"
-                                    placeholder="Insert employee ID..."
+                                    placeholder="Insert employee ID..." id={"empID"}
                                 />
                             </td>
                         </tr>
                     </table>
+                    </div>
                 </form>
 
 

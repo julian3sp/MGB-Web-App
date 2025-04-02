18acacd6b3a96fe3226e61b28c84ed6d29629e69
jacksoncolegrove <jc.coolgroove@gmail.com>
2025-04-01 20:09:49 -0400

    no real changes need to pull

--- apps/frontend/src/components/serviceRequest/PersonalInfoForm.tsx
+++ apps/frontend/src/components/serviceRequest/PersonalInfoForm.tsx
@@ -1,27 +1,35 @@
 import './serviceForm.css'
+import {useState} from "react";
 
 function PersonalInformationForm() {
+    const [text, setText] = useState("")
     return (
         <div className="form-container">
             <form className="service-form">
                 <input
+                    value = "fullName"
                     className = {"service-request"}
-                    placeholder={"insert email ..."}
+                    placeholder={"insert email . . ."}
                 />
                 <input
+                    id = "email"
+                    name = "email"
+                    value = "email"
                     className = {"service-request"}
-                    placeholder={"insert full name ..."}
+                    placeholder={"insert full name . . ."}
                 />
 
             </form>
             <form className="service-form">
                 <input
+                    value = "phoneNumber"
                     className = {"service-request"}
-                    placeholder={"insert phone number ..."}
+                    placeholder={"insert phone number . . ."}
                 />
                 <input
+                    value = "employeeID"
                     className = {"service-request"}
-                    placeholder={"insert employee ID ..."}
+                    placeholder={"insert employee ID . . ."}
                 />
             </form>
         </div>

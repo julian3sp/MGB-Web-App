a76ebb09509d94aca11c9b25632ac44fb9e66b53
julian3sp <julianrespinal24@gmail.com>
2025-04-02 12:12:36 -0400

    new input and text components, language request form finished

--- apps/frontend/src/routes/ServiceRequestPage.tsx
+++ apps/frontend/src/routes/ServiceRequestPage.tsx
@@ -1,23 +1,16 @@
-import PersonalInfoForm from "../components/serviceRequest/PersonalInfoForm.tsx";
-import ServiceRequestForm from "../components/serviceRequest/ServiceRequestForm.tsx";
+
+import LanguageRequestForm from "../components/serviceRequest/LanguageRequestForm.tsx";
 import {useState} from "react";
 
 
 function ServiceRequestPage(){
-    const [form, setFormData] = useState(
-        {
-            fullName: '',
-            email: '',
-            phoneNumber: '',
-            employeeID: ''
-        })
+
     return(
         <>
             <div className="bg-gray-200 min-h-screen flex flex-col items-center justify-center">
                 <h1 className="text-2xl font-bold mb-6">Service Request Page</h1>
                 <div className="bg-white p-6 rounded-lg w-[900px] mx-auto shadow-lg">
-                    <PersonalInfoForm formData={form} setFormData={setFormData} />
-                    <ServiceRequestForm />
+                    <LanguageRequestForm/>
                 </div>
             </div>
 

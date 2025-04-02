2517743657fb005827df20526ece256d6631d0dd
bjpeters2027 <brendonpeters2023@gmail.com>
2025-04-02 13:29:38 -0400

    Styling

--- apps/frontend/src/routes/SignInPage.tsx
+++ apps/frontend/src/routes/SignInPage.tsx
@@ -10,22 +10,19 @@ export default function SignInPage() {
 
     if (hasAccount) {
         return (
-            <div className={"SignInPage"}>
-                <div className={"SignInForm"}>
+            <div className="flex justify-center items-center w-screen h-screen bg-[radial-gradient(circle_at_top_left,white,#003a96)]">
+                <div className="m-auto py-[42px] px-[72px] bg-[white] rounded-[30px] w-[540px] h-[516px]">
                     <SignInForm/>
                     <button onClick={() => setHasAccount(false)}>Don't have an account? Create one!</button>
                 </div>
             </div>
         )
     }
+
     return (
         <div className={"SignInPage"}>
             <div className={"SignInForm"}>
                 <CreateAccountForm/>
-    return (
-        <div className="flex justify-center items-center w-screen h-screen bg-[radial-gradient(circle_at_top_left,white,#003a96)]">
-            <div className="m-auto py-[42px] px-[72px] bg-[white] rounded-[30px] w-[540px] h-[516px]">
-                <SignInForm/>
             </div>
         </div>
     )

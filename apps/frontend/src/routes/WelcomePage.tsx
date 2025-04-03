df18f7f7d0bfc7b9ed5198695a2c369247c0ddb2
KZhang101 <originalsuperarmd@gmail.com>
2025-04-02 21:02:55 -0400

    welcome page added

---
+++ apps/frontend/src/routes/WelcomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UncontrolledExample from "../components/carousel";
+
+const WelcomePage = () => {
+    return (
+        <UncontrolledExample>
+            
+        </UncontrolledExample>
+    )
+}
+
+export default WelcomePage;

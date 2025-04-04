7898915b9424e16ef40af691a5c84b256c6ef265
julian3sp <julianrespinal24@gmail.com>
2025-04-04 13:45:10 -0400

    deleting branch

--- apps/backend/src/server/index.ts
+++ apps/backend/src/server/index.ts
@@ -2,13 +2,7 @@ import { router, publicProcedure } from './trpc.ts'
 import PrismaClient from "../bin/prisma-client.ts";
 import prismaClient from "../bin/prisma-client.ts";
 
-const appRouter = router({
-    requestList: publicProcedure
-        .query( async () => {
-            const requests = await prismaClient.service_requests.findMany()
-            return requests;
-        })
-})
+
 
 export type appRouter = typeof appRouter;
 

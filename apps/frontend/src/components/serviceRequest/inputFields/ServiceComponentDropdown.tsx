21470a97ef5420b6e2635d219aeaafb56a3e49ca
Conor-McCoy <conormccoy256@gmail.com>
2025-04-14 18:48:44 -0400

    Updated table & list content/styling

--- apps/frontend/src/components/serviceRequest/inputFields/ServiceComponentDropdown.tsx
+++ apps/frontend/src/components/serviceRequest/inputFields/ServiceComponentDropdown.tsx
@@ -48,12 +48,10 @@ export function ServiceComponentDropdown({
                     setState(e.target.value);
                 }}
             >
-
                 <option value ="" disabled hidden className="text-gray-500">{placeholder}</option>
 
                 {options.map((option) => (
                     <option value={option}>{option}</option>
-
                 ))}
             </select>
         </div>

20baaf596bf137b5b59a16904394bd02646c51d1
jacksoncolegrove <jc.coolgroove@gmail.com>
2025-04-14 20:23:56 -0400

    security service request form

--- apps/backend/src/server/procedures/requests.ts
+++ apps/backend/src/server/procedures/requests.ts
@@ -5,12 +5,14 @@ import { trpc } from '../trpc.ts';
 
 export const getRequests = trpc.procedure.query(async () => {
     console.log('getRequests called');
-    const requests = await client.service_request.findMany(
-        {include: {
+    const requests = await client.service_request.findMany({
+        include: {
             sanitation: true,
             language: true,
-        },}
-    );
+            security: true,
+            audioVisual: true,
+        },
+    });
     console.log('getRequests returned');
     return requests;
 });
@@ -26,21 +28,25 @@ export const makeRequest = publicProcedure
             status: z.string(),
             request_type: z.string(),
             additional_comments: z.optional(z.string()),
-            sanitation: z.optional(z.object(
-                {
+            sanitation: z.optional(
+                z.object({
                     cleaningType: z.string(),
                     contaminant: z.string(),
-                }
-            )),
-            language: z.optional(z.object(
-                {
+                })
+            ),
+            language: z.optional(
+                z.object({
                     sourceLanguage: z.string(),
                     targetLanguage: z.string(),
-                }
-            )),
-            audioVisual: z.optional(z.object(
-                { accommodations: z.string() }
-            )),
+                })
+            ),
+            security: z.optional(
+                z.object({
+                    accessZones: z.string(),
+                    securityIssue: z.string(),
+                })
+            ),
+            audioVisual: z.optional(z.object({ accommodations: z.string() })),
         })
     )
     .mutation(async ({ input }) => {
@@ -48,13 +54,16 @@ export const makeRequest = publicProcedure
             data: {
                 ...input,
                 sanitation: {
-                    create: input.sanitation
+                    create: input.sanitation,
                 },
                 language: {
-                    create: input.language
+                    create: input.language,
                 },
                 audioVisual: {
-                    create: input.audioVisual
+                    create: input.audioVisual,
+                },
+                security: {
+                    create: input.security,
                 },
             },
         });

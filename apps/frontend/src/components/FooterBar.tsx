b1d7b096c4c3e88002ec3e4d22702258c8812d3d
Ayush <ayushk2023@gmail.com>
2025-04-15 21:02:55 -0400

    Make LogIn/LogOut for Footer Bar and change font to poppins

--- apps/frontend/src/components/FooterBar.tsx
+++ apps/frontend/src/components/FooterBar.tsx
@@ -3,13 +3,15 @@ import React from 'react'
 import {Link} from "react-router-dom"
 import { SocialIcon } from 'react-social-icons'
 import ReactDOM from 'react-dom'
+import {LogInButton} from "@/components/signIn/LogInButton.tsx";
+import {LogOutButton} from "@/components/signIn/LogOutButton.tsx";
 
 function FooterBar() {
     const [tab, setTab] = React.useState<string>("");
     return (
         <div className="bg-[#003a96] text-white p-5">
             <nav className="flex justify-between items-center text-white">
-                <div className="flex items-center space-x-20 text-xl">
+                <div className="flex items-center space-x-20 text-xl font-[Poppins]">
                     <div className="flex">
                         <Link to="" onClick={() => setTab("")}>
                             <img src={HospitalIcon} alt="Mass General Brigham Logo"  className="width-20"/>
@@ -35,10 +37,9 @@ function FooterBar() {
                             Navigation
                         </Link>
                     </div>
-                    <div className="flex">
-                        <Link to="/signIn" onClick={() => setTab("")}>
-                            Login
-                        </Link>
+                    <div className="flex space-x-20">
+                        <LogInButton className="text-xl"/>
+                        <LogOutButton className="text-xl"/>
                     </div>
                 </div>
                 <div>
@@ -49,7 +50,7 @@ function FooterBar() {
                     <SocialIcon url="https://www.linkedin.com/company/mass-general-brigham" bgColor="#003a96"/>
                 </div>
             </nav>
-            <div className="py-5">
+            <div className="py-5 font-[Poppins]">
                 This page is not to be used for emergencies. For emergencies, please dial 911
             </div>
         </div>
--- apps/frontend/src/components/signIn/LogInButton.tsx
+++ apps/frontend/src/components/signIn/LogInButton.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
-export function LogInButton(){
+export function LogInButton({className}: {className?: string}) {
     const { loginWithRedirect, isAuthenticated } = useAuth0();
     return (
         !isAuthenticated && (
             <button
-                className="group text-sm px-5 py-5 transition-all duration-150 ease-in-out bg-[#003a96] text-white"
+                className={`group px-5 py-5 transition-all duration-150 ease-in-out bg-[#003a96] text-white font-[Poppins] ${className ?? ''}`}
                 onClick={() => loginWithRedirect()}>Log In
             </button>
         )

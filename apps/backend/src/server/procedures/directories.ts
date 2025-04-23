7346aa33237ddd960f629e4e523521393bf5bf44
Conor-McCoy <conormccoy256@gmail.com>
2025-04-23 11:48:12 -0400

    removed leftover node from directories.ts

--- apps/backend/src/server/procedures/directories.ts
+++ apps/backend/src/server/procedures/directories.ts
@@ -32,7 +32,6 @@ export const makeDirectories = publicProcedure
             services: z.string(),
             location: z.string(),
             telephone: z.string(),
-            node: z.number(),
         })
     )
     .mutation(async ({ input }) => {

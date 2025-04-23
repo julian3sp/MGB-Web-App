2b78841912da564f662bb909c0ef03bff20132a3
bjpeters2027 <brendonpeters2023@gmail.com>
2025-04-23 03:06:51 -0400

    Nav Bar not weird

--- apps/frontend/src/components/NavBar.tsx
+++ apps/frontend/src/components/NavBar.tsx
@@ -5,7 +5,6 @@ import '../styles/mainStyles.css'
 import {LogInButton} from "./signIn/LogInButton.tsx";
 import {LogOutButton} from "./signIn/LogOutButton.tsx"
 import { useAuth0 } from "@auth0/auth0-react";
-import icon from "../../assets/logo-icon-mass-general-brigham.png";
 
 
 type Props = {
@@ -22,58 +21,73 @@ export default function NavBar({loginTag, isSignedIn, signOut}: Props) {
     const isLoginPage = location.pathname === "/signIn" || location.pathname === "/createAcc";
     const [isAdmin, setAdmin] = React.useState<boolean>(false);
 
+    useEffect(() => {
+        if (location.pathname === "/navigation") {
+            const navbar = document.getElementById("navbar") as HTMLElement;
+            if (navbar) {
+                navbar.style.display = "none";
+            }
+            window.scrollTo(0, 65);
+        } else {
+            const navbar = document.getElementById("navbar") as HTMLElement;
+            if (navbar) {
+                navbar.style.display = "block";
+            }
+        }
+    }, [location.pathname]);
+
     return (
         <nav className="flex justify-between items-center bg-white text-white border-b-1 border-gray-300">
             <div className="flex items-center space-x-4">
-                <Link to={"/"} className={"ml-5 flex items-center space-x-2 text-sm text-[#003a96] font-[Poppins]"} onClick={() => setTab("")}>
-                    <img src={icon} alt="Mass General Brigham Logo"  className="h-6"/>
-                    <div className="flex font-semibold">Mass General Brigham</div>
+                <Link to={"/"} className={"ml-5"} onClick={() => setTab("")}>
+                    <img src={logo} alt="Mass General Brigham Logo"  className="h-6"/>
                 </Link>
                 <div className="flex">
                     <Link to="/directory" onClick={() => setTab("dir")}
                           className={tab === "dir" ?
-                              "text-base bg-[#003a96] font-[Poppins] text-white px-5 py-5" :
-                              "text-base text-black hover:bg-[#003a96] font-[Poppins] hover:text-white px-5 py-5 transition-all"}>
+                              "bg-[#003a96] font-[Poppins] text-white  px-5 py-5" :
+                              "text-base text-black hover:bg-[#003a96] font-[Poppins] hover:text-white  px-5 py-5 transition-all"}>
                         Directory
                     </Link>
                     <Link to="/navigation" onClick={() => setTab("navigation")}
                           className={tab === "navigation" ?
-                              "text-base bg-[#003a96] font-[Poppins] text-white  px-5 py-5" :
+                              "bg-[#003a96] font-[Poppins] text-white  px-5 py-5" :
                               "text-base text-black hover:bg-[#003a96] font-[Poppins] hover:text-white  px-5 py-5 transition-all"}>
                         Navigation
                     </Link>
                     <div className="flex">
                         {isAuthenticated ? <Link to="/services" onClick={() => setTab("serv")}
-                          className={tab === "serv" ?
-                              "text-base bg-[#003a96] font-[Poppins] text-white  px-5 py-5" :
-                              "text-base text-black hover:bg-[#003a96] font-[Poppins] hover:text-white  px-5 py-5 transition-all"}>
-                        Services
+                                                 className={tab === "serv" ?
+                                                     "bg-[#003a96] font-[Poppins] text-white  px-5 py-5" :
+                                                     "text-base text-black hover:bg-[#003a96] font-[Poppins] hover:text-white  px-5 py-5 transition-all"}>
+                            Services
                         </Link> : null}
                         {isAuthenticated ? <Link to="/requests" onClick={() => setTab("reqP")}
-                          className={tab === "reqP" ?
-                              "text-base bg-[#003a96] font-[Poppins] text-white  px-5 py-5" :
-                              "text-base text-black hover:bg-[#003a96] font-[Poppins] hover:text-white  px-5 py-5 transition-all"}>
-                        View Requests
+                                                 className={tab === "reqP" ?
+                                                     "bg-[#003a96] font-[Poppins] text-white  px-5 py-5" :
+                                                     "text-base text-black hover:bg-[#003a96] font-[Poppins] hover:text-white  px-5 py-5 transition-all"}>
+                            View Requests
                         </Link> : null}
                         {isAdmin || (isAuthenticated && (window.sessionStorage.getItem("isAdmin") === "true")) ? <Link to="/editor" onClick={() => setTab("editor")}
-                          className={tab === "editor" ?
-                              "text-base bg-[#003a96] font-[Poppins] text-white  px-5 py-5" :
-                              "text-base text-black hover:bg-[#003a96] font-[Poppins] hover:text-white  px-5 py-5 transition-all"}>
-                        Map Editor
+                                                                                                                       className={tab === "editor" ?
+                                                                                                                           "bg-[#003a96] font-[Poppins] text-white  px-5 py-5" :
+                                                                                                                           "text-base text-black hover:bg-[#003a96] font-[Poppins] hover:text-white  px-5 py-5 transition-all"}>
+                            Map Editor
                         </Link> : null}
                     </div>
                     <div className="flex">
-                        {isAdmin || (isAuthenticated && (window.sessionStorage.getItem("isAdmin") === "true")) ? <Link to="/admin/directory" onClick={() => setTab("exp")}
-                          className={tab === "exp" ?
-                              "text-base bg-[#003a96] font-[Poppins] text-white  px-5 py-5" :
-                              "text-base text-black hover:bg-[#003a96] font-[Poppins] hover:text-white  px-5 py-5 transition-all"}>
-                        Export
-                    </Link> : null }
+                        {isAdmin || (isAuthenticated && (window.sessionStorage.getItem("isAdmin") === "true")) ?
+                            <Link to="/admin/directory" onClick={() => setTab("exp")}
+                                  className={tab === "exp" ?
+                                      "text-base bg-[#003a96] font-[Poppins] text-white  px-5 py-5" :
+                                      "text-base text-black hover:bg-[#003a96] font-[Poppins] hover:text-white  px-5 py-5 transition-all"}>
+                                Export
+                            </Link> : null }
                     </div>
-                    <LogInButton className=" right-0 text-base text-black" rerender={setAdmin}/>
-                    <LogOutButton className=" right-0 text-base text-black"/>
                 </div>
             </div>
+            <LogInButton className= "text-base text-black" rerender={setAdmin}/>
+            <LogOutButton className="text-base text-black"/>
         </nav>
     )
 }

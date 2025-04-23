2447a8a0e42130a4cb08f310439bcdc4b528768c
bjpeters2027 <brendonpeters2023@gmail.com>
2025-04-23 19:56:21 -0400

    strategy

--- apps/frontend/src/components/navigation/pathfinding/Graph.ts
+++ apps/frontend/src/components/navigation/pathfinding/Graph.ts
@@ -31,7 +31,7 @@ type Edit = {
 
 export class Graph {
     private nodes: Set<Node>;
-    private adjacencyList: Map<Node, Edge[]>;
+    adjacencyList: Map<Node, Edge[]>;
     private edges: Edge[];
     private edits: Edit;
 
@@ -213,10 +213,6 @@ export class Graph {
         }
     }
 
-    getNeighbors(node: Node): Edge[] {
-        return this.adjacencyList.get(node) || [];
-    }
-
     getNode(id: number): Node | undefined {
         return Array.from(this.nodes).find((node) => node.id === id);
     }
@@ -274,144 +270,6 @@ export class Graph {
         );
     }
 
-    BFS(startNode: Node, targetNode: Node): Node[] {
-        const visited: Node[] = [];
-        const queue: Node[] = [startNode];
-        const path: Node[] = [];
-
-        startNode.parent = undefined;
-
-        while (queue.length > 0) {
-            const currentNode: Node | undefined = queue.shift(); // Same as real pop first element
-            if (currentNode === undefined) break; // TypeScript weird stuff bruh
-            if (!visited.includes(currentNode)) {
-                visited.push(currentNode);
-                //targetId is found
-                if (currentNode === targetNode) break;
-
-                const neighbors: Edge[] = this.getNeighbors(currentNode); // Edges
-                for (const edge of neighbors) {
-                    const neighbor = edge.targetId;
-                    if (!visited.includes(neighbor)) {
-                        neighbor.parent = currentNode;
-                        queue.push(neighbor);
-                    }
-                }
-            }
-        }
-
-        return this.reCreatePath(targetNode);
-    }
-
-    DFS(startNode: Node, targetNode: Node): Node[] {
-        const visited: Node[] = [];
-        const stack: Node[] = [startNode];
-
-        startNode.parent = undefined;
-
-        while (stack.length > 0) {
-            const currentNode = stack.pop();
-            if (!currentNode) break;
-
-            if (!visited.includes(currentNode)) {
-                visited.push(currentNode);
-
-                if (currentNode === targetNode) break;
-
-                const neighbors = this.getNeighbors(currentNode);
-
-                for (let i = neighbors.length - 1; i >= 0; i--) {
-                    const neighbor = neighbors[i].targetId;
-                    if (!visited.includes(neighbor)) {
-                        neighbor.parent = currentNode;
-                        stack.push(neighbor);
-                    }
-                }
-            }
-        }
-
-        return this.reCreatePath(targetNode);
-    }
-
-    heuristicCost(startNode: Node, targetNode: Node): number {
-        /**
-         * Calculate the heuristic cost for current node
-         */
-        return Math.sqrt(
-            Math.pow(targetNode.x - startNode.x, 2) + Math.pow(targetNode.y - startNode.y, 2)
-        );
-    }
-
-    getLowestCostNode(nodes: Node[]): Node {
-        /**
-         * Returns the lowest code node from a list of nodes
-         */
-        const costs: number[] = [];
-        for (const node of nodes) costs.push(node.totalCost);
-
-        return nodes[costs.indexOf(Math.min(...costs))];
-    }
-
-    reCreatePath(node: Node): Node[] {
-        /**
-         * Recreates the path given parent nodes of current node
-         */
-        const path: Node[] = [];
-        let currentNode: Node = node;
-        while (currentNode !== undefined) {
-            path.unshift(currentNode);
-            if (currentNode.parent === undefined) {
-                break;
-            } else {
-                currentNode = currentNode.parent;
-            }
-        }
-        return path;
-    }
-
-    aStar(startNode: Node, targetNode: Node): Node[] {
-        const evaluate: Node[] = [startNode];
-        const finished: Node[] = [];
-
-        startNode.edgeCost = 0;
-        startNode.totalCost = this.heuristicCost(startNode, targetNode);
-        startNode.parent = undefined;
-
-        while (evaluate.length > 0) {
-            const currentNode: Node = this.getLowestCostNode(evaluate);
-
-            if (currentNode === targetNode) return this.reCreatePath(currentNode);
-
-            // Update evaluate and finished
-            const currentIndex: number = evaluate.indexOf(currentNode);
-            evaluate.splice(currentIndex, 1);
-            finished.push(currentNode);
-
-            const neighbors: Edge[] = this.getNeighbors(currentNode); // Edges
-            for (const edge of neighbors) {
-                const neighbor: Node = edge.targetId;
-                //skip node if checked
-                if (finished.includes(edge.targetId)) continue;
-
-                // cost of moving to new node
-                const currentEdgeCost: number = neighbor.edgeCost + edge.weight;
-
-                // Check for new node
-                if (!evaluate.includes(neighbor)) {
-                    evaluate.push(neighbor);
-                }
-                // Check if current path is better
-                else if (currentEdgeCost >= neighbor.edgeCost) {
-                    continue;
-                }
-
-                neighbor.parent = currentNode;
-                neighbor.edgeCost = currentEdgeCost;
-                neighbor.totalCost = currentEdgeCost + this.heuristicCost(neighbor, targetNode);
-            }
-        }
-        return []; // No path found, should be impossible
-    }
 }
 
 export default Graph;

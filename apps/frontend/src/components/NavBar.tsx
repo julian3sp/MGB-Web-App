aae46377abb23fcd5ca97e0c2344e566415bf32a
shahsxd <alexlaredo2212@gmail.com>
2025-04-29 00:04:12 -0400

    toggle

--- apps/frontend/src/components/NavBar.tsx
+++ apps/frontend/src/components/NavBar.tsx
@@ -12,87 +12,97 @@ type Props = {
     setUserRole: (newRole: string) => void
 }
 
-const speech = window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new speech();
-recognition.continuous = true;
-recognition.interimResults = true;
-recognition.start();
+    const speech = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const recognition = new speech();
+    recognition.continuous = true;
+    recognition.interimResults = true;
 
-export default function NavBar({ userRole,  setUserRole }: Props) {
-    const location = useLocation();
-    const navigate = useNavigate();
-    const [tab, setTab] = React.useState<string>(() => {
-        const path = location.pathname;
-        if(path.startsWith("/directory")) return "dir"; 
-        if(path.startsWith("/navigation")) return "navigation";
-        if(path.startsWith("/services")) return "serv";
-        if(path.startsWith("/requests")) return "reqP";
-        if(path.startsWith("/editor")) return "editor";
-        if(path.startsWith("/admin/directory")) return "exp";
-        return "";
-    })
-    const { isAuthenticated, logout } = useAuth0();
 
-    const voiceCommands = () => {
-        recognition.onstart = () => {
-            console.log("Starting voice commands");
-        };
+    export default function NavBar({ userRole,  setUserRole }: Props) {
+        const location = useLocation();
+        const navigate = useNavigate();
+        const [tab, setTab] = React.useState<string>(() => {
+            const path = location.pathname;
+            if(path.startsWith("/directory")) return "dir";
+            if(path.startsWith("/navigation")) return "navigation";
+            if(path.startsWith("/services")) return "serv";
+            if(path.startsWith("/requests")) return "reqP";
+            if(path.startsWith("/editor")) return "editor";
+            if(path.startsWith("/admin/directory")) return "exp";
+            return "";
+        })
+        const { isAuthenticated, logout } = useAuth0();
+        const [disableVoice, setDisableVoice] = useState(false);
 
-        recognition.onresult = (e) => {
-            let current = e.resultIndex;
-            let transcript = e.results[current][0].transcript.trim();
-
-            if (transcript.toLowerCase().includes("navigation") ) {
-                navigate("/navigation");
-            } else if (transcript.toLowerCase().includes("home") ) {
-                navigate("/");
-            } else if (transcript.toLowerCase().includes("directory") || transcript.toLowerCase().includes("department")) {
-                navigate("/directory");
-            } else if (transcript.toLowerCase().includes("home") ) {
-                navigate("/");
-            }
-        };
-
-        recognition.onerror = (event) => {
-            console.error("Speech recognition error", event.error);
-            // Restart recognition if an error occurs
-            setTimeout(() => {
-                recognition.start();
-            }, 100);
-        };
-
-        recognition.onend = () => {
-            console.log("Speech recognition ended, restarting...");
-            // Automatically restart when recognition ends
-            setTimeout(() => {
-                recognition.start();
-            }, 100);
-        };
-
-    };
-
-    useEffect(() => {
-        voiceCommands();
-        return () => {
-            // Cleanup: stop recognition when component unmounts
-            recognition.stop();
-        };
-    }, []);
-
-    useEffect(() => {
-        if (location.pathname === "/navigation") {
-            const navbar = document.getElementById("navbar") as HTMLElement;
-            if (navbar) {
-                navbar.style.display = "none";
-            }
-            window.scrollTo(0, 65);
-        } else {
-            const navbar = document.getElementById("navbar") as HTMLElement;
-            if (navbar) {
-                navbar.style.display = "block";
-            }
+        const handleDisable = () => {
+            setDisableVoice(prev => !prev);
+            console.log("disableVoice:", !disableVoice);
         }
-    }, [location.pathname]);
+
+
+        const voiceCommands = () => {
+
+            recognition.onstart = () => {
+                console.log("Starting voice commands");
+            };
+
+            recognition.onresult = (e) => {
+                let current = e.resultIndex;
+                let transcript = e.results[current][0].transcript.trim();
+
+                if (transcript.toLowerCase().includes("navigation") ) {
+                    navigate("/navigation");
+                } else if (transcript.toLowerCase().includes("home") ) {
+                    navigate("/");
+                } else if (transcript.toLowerCase().includes("directory") || transcript.toLowerCase().includes("department")) {
+                    navigate("/directory");
+                } else if (transcript.toLowerCase().includes("home") ) {
+                    navigate("/");
+                }
+            };
+
+            recognition.onerror = (event) => {
+                console.error("Speech recognition error", event.error);
+                // Restart recognition if an error occurs
+                setTimeout(() => {
+                    recognition.stop()
+                }, 100);
+            };
+
+            recognition.onend = () => {
+                console.log("Speech recognition ended, restarting...");
+                // Automatically restart when recognition ends
+                setTimeout(() => {
+                    recognition.stop()
+                }, 100);
+            };
+
+        };
+
+        useEffect(() => {
+            voiceCommands();
+            return () => {
+                // Cleanup: stop recognition when component unmounts
+                recognition.start();
+            };
+        }, []);
+
+        useEffect(() => {
+            if (location.pathname === "/navigation") {
+                const navbar = document.getElementById("navbar") as HTMLElement;
+                if (navbar) {
+                    navbar.style.display = "none";
+                }
+                window.scrollTo(0, 65);
+            } else {
+                const navbar = document.getElementById("navbar") as HTMLElement;
+                if (navbar) {
+                    navbar.style.display = "block";
+                }
+            }
+        }, [location.pathname]);
+
+
 
     return (
         <nav className="flex justify-between items-center bg-white text-white border-b-1 border-gray-300">
@@ -132,7 +142,14 @@ export default function NavBar({ userRole,  setUserRole }: Props) {
                                    "text-base text-black hover:bg-[#003a96] font-[Poppins] hover:text-white  px-5 py-5 transition-all"}>
                             Map Editor
                         </Link> : null}
+                        <button onClick={handleDisable}
+                                className="text-black bg-white hover:bg-[#003a96] hover:text-white font-[Poppins] px-5 py-5 transition-all ">
+                            Enable Voice
+                        </button>
+
                     </div>
+
+
                     <div className="flex">
                         { (isAuthenticated && userRole === "Admin") ?
                             <Link to="/admin/directory" onClick={() => setTab("exp")}

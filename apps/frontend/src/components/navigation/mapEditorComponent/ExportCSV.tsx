3ea54d5af87e59a5b8d000a5e468c4b12d4dede0
julian3sp <julianrespinal24@gmail.com>
2025-04-30 10:16:49 -0400

    admin page accents

--- apps/frontend/src/components/navigation/mapEditorComponent/ExportCSV.tsx
+++ apps/frontend/src/components/navigation/mapEditorComponent/ExportCSV.tsx
@@ -117,7 +117,7 @@ export default function ExportCSV({type} : ExportCSVProps) {
         <div className=" px-6  max-w-md">
           <button
             onClick={downloadCSV}
-            className="w-full bg-white border-2 border-[#bbbbbb] text-[#003a96] font-[poppins] py-2 rounded-lg hover:bg-accent"
+            className="w-full bg-white border-1 shadow-sm border-[#44A6A6] text-[#003a96] font-[poppins] py-2 rounded-lg hover:bg-[#003a96] hover:text-white"
           >
             Export {type}
           </button>

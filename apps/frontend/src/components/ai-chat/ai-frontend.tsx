7c4ebdabd348f552df6a5d7109bfdaa6f9ca0ca7
shahsxd <alexlaredo2212@gmail.com>
2025-05-03 12:54:47 -0400

    Ai chat bot no python no venv

--- apps/frontend/src/components/ai-chat/ai-frontend.tsx
+++ apps/frontend/src/components/ai-chat/ai-frontend.tsx
@@ -6,6 +6,7 @@ const api_key = import.meta.env.VITE_GOOGLE_API_KEY;
 if (!api_key) {
     console.error("Missing API key. Please check your .env.local file");
 }
+//dont use ai backend
 
 const genAI = new GoogleGenerativeAI(api_key, { apiVersion: 'v1' });
 

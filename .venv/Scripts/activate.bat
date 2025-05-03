1542804e45a7ae9d92cd54ee2d9cd7645c3b8fa8
julian3sp <julianrespinal24@gmail.com>
2025-05-02 20:31:59 -0400

    working

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\brend\Downloads\CS-3733\Final-Project\team-f\.venv
+set VIRTUAL_ENV=C:\Users\julia\WebstormProjects\team--f\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
